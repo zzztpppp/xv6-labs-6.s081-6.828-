070bb51cc2f808fa245c2d60a53844b64a400896
azu <cxztpcom@gmail.com>
2021-05-13 15:12:13 +0800

    Implement primes with CSP model

---
+++ user/primes.c
@@ -0,0 +1,73 @@
+// primes - Implement a prime sieve via multi-processsing and pipes.
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user.h"
+
+#define START 2
+#define END 35
+
+
+int sieve(int read_fd, int modulo) {
+
+    int p[2], pid, n, data;
+    char *buf[4];
+    int mod;
+
+    if (modulo == END + 1) {  // End of the process
+        close(read_fd);
+        exit(0);
+    }
+    else {
+        pipe(p);
+        if ((pid = fork()) == 0) {    // Create a right neighbor at the pipe
+            close(read_fd);
+            close(p[1]);
+            sieve(p[0], modulo + 1);
+        }
+        else {
+            close(p[0]);
+            while ((n = read(read_fd, buf, 4)) > 0) {
+                data = *(int *)buf;
+                mod = data % modulo;     
+                if (mod == 0 && data == modulo) {
+                    printf("%d\n", data);
+                }
+                else if (mod == 0 && data != modulo)
+                {
+                    continue;
+                }
+                else {
+                    write(p[1], buf, 4);
+                }
+            }
+        }
+        close(read_fd);
+        close(p[1]);
+        wait(&pid);
+    }
+    exit(0);
+}
+
+int main(int argc, char *argv[]) {
+    int p[2], i, pid;
+    if (pipe(p) < 0) {
+        fprintf(2, "Error on opening a pipe");
+        exit(1);
+    }
+
+    if ((pid = fork()) == 0) {
+        // Parent process only needs to write.
+        close(p[0]);
+        for (i = START; i < 35; i++) {
+            write(p[1], &i, 4);
+        }
+    }
+    else {
+        // Child only needs to read.
+        close(p[1]);
+        sieve(p[0], START);
+    }
+    close(p[1]);
+    wait(&pid);
+    exit(0);
+}
