41a457863f9ee06cce18a123ddc92cadfe186bf3
azu <cxztpcom@gmail.com>
2021-05-25 15:33:55 +0800

    Implement sys_sysinfo

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -6,6 +6,7 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
+#include "sysinfo.h"
 
 uint64
 sys_exit(void)
@@ -103,5 +104,24 @@ sys_trace(void)
   if (argint(0, &scall) < 0)
       return -1;
   myproc()->trace_mask = scall;
+  return 0;
+}
+
+uint64
+sys_sysinfo(void)
+{
+  struct sysinfo info;
+  struct proc *p = myproc();
+  uint64 addr;    // User specified addr for info
+
+  if (argaddr(0, &addr) < 0)
+      return -1;
+
+  info.nproc = used_nproc();
+  info.freemem = nfree();
+
+  // Copy the content of info to userspace.
+  if(copyout(p->pagetable, addr, (char *)&info, sizeof(info)) < 0)
+    return -1;
   return 0;
 }
