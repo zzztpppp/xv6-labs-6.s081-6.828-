062567723ab944e8c52b7a1ef7ac385779e4e032
azu <cxztpcom@gmail.com>
2021-05-25 10:58:28 +0800

    Remove system call name prefix. Reformat printing

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -132,28 +132,28 @@ static uint64 (*syscalls[])(void) = {
 };
 
 static char *syscall_names[] = {
-[SYS_fork]    "sys_fork",
-[SYS_exit]    "sys_exit",
-[SYS_wait]    "sys_wait",
-[SYS_pipe]    "sys_pipe",
-[SYS_read]    "sys_read",
-[SYS_kill]    "sys_kill",
-[SYS_exec]    "sys_exec",
-[SYS_fstat]   "sys_fstat",
-[SYS_chdir]   "sys_chdir",
-[SYS_dup]     "sys_dup",
-[SYS_getpid]  "sys_getpid",
-[SYS_sbrk]    "sys_sbrk",
-[SYS_sleep]   "sys_sleep",
-[SYS_uptime]  "sys_uptime",
-[SYS_open]    "sys_open",
-[SYS_write]   "sys_write",
-[SYS_mknod]   "sys_mknod",
-[SYS_unlink]  "sys_unlink",
-[SYS_link]    "sys_link",
-[SYS_mkdir]   "sys_mkdir",
-[SYS_close]   "sys_close",
-[SYS_trace]   "sys_trace"
+[SYS_fork]    "fork",
+[SYS_exit]    "exit",
+[SYS_wait]    "wait",
+[SYS_pipe]    "pipe",
+[SYS_read]    "read",
+[SYS_kill]    "kill",
+[SYS_exec]    "exec",
+[SYS_fstat]   "fstat",
+[SYS_chdir]   "chdir",
+[SYS_dup]     "dup",
+[SYS_getpid]  "getpid",
+[SYS_sbrk]    "sbrk",
+[SYS_sleep]   "sleep",
+[SYS_uptime]  "uptime",
+[SYS_open]    "open",
+[SYS_write]   "write",
+[SYS_mknod]   "mknod",
+[SYS_unlink]  "unlink",
+[SYS_link]    "link",
+[SYS_mkdir]   "mkdir",
+[SYS_close]   "close",
+[SYS_trace]   "trace"
 };
 
 void
@@ -168,7 +168,7 @@ syscall(void)
 
     // Print the system call's return value if traced.
     if (p->trace_mask & (1 << num)) {
-      printf("%d syscall %s -> %d\n", p->pid, 
+      printf("%d: syscall %s -> %d\n", p->pid, 
              syscall_names[num], p->trapframe->a0);
     }
   } else {
